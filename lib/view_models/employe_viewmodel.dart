4222b126d1a2e015483f4c4f9cca74651b48ca94
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2025-12-19 11:30:50 +0545

    11 30

--- lib/view_models/employe_viewmodel.dart
+++ lib/view_models/employe_viewmodel.dart
@@ -1,1 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_starter/models/employe_model.dart';
+import 'package:riverpod_starter/state/employe_state.dart';
 
+class EmployeViewmodel extends Notifier<EmployeState> {
+  @override
+  EmployeState build() {
+    return EmployeState();
+  }
+
+  Future<void> addEmployes(EmployeeModel employe) async {
+    state = state.copyWith(isLoading: true);
+    await Future.delayed(Duration(seconds: 2));
+
+    final updatedEmployesList = [...state.lstEmployes, employe];
+    state = state.copyWith(isLoading: false, lstEmployes: updatedEmployesList);
+  }
+
+  Future<void> loadEmployes() async {
+    state = state.copyWith(isLoading: true);
+    await Future.delayed(Duration(seconds: 2));
+    state = state.copyWith(isLoading: false);
+  }
+}
--- lib/view_models/student_viewmodel.dart
+++ lib/view_models/student_viewmodel.dart
@@ -4,13 +4,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_starter/state/student_state.dart';
 import 'package:riverpod_starter/models/student_model.dart';
 
-// 1. NotifierProvider
 final studentViewModelProvider =
     NotifierProvider<StudentViewmodel, StudentState>(() {
       return StudentViewmodel();
     });
 
-// 2. ViewModel
 class StudentViewmodel extends Notifier<StudentState> {
   @override
   StudentState build() {
@@ -18,12 +16,9 @@ class StudentViewmodel extends Notifier<StudentState> {
     return StudentState(isLoading: false, lstStudents: []);
   }
 
-  // 3. Add a student
   Future<void> addStudent(StudentModel student) async {
-    // Set loading to true
     state = state.copyWith(isLoading: true);
 
-    // Simulate delay
     await Future.delayed(Duration(seconds: 2));
 
     // Update the student list
@@ -31,11 +26,9 @@ class StudentViewmodel extends Notifier<StudentState> {
     state = state.copyWith(isLoading: false, lstStudents: updatedStudents);
   }
 
-  // 4. Load students
   Future<void> loadStudents() async {
     state = state.copyWith(isLoading: true);
 
-    // Simulate fetching from API
     await Future.delayed(Duration(seconds: 2));
 
     state = state.copyWith(isLoading: false);
